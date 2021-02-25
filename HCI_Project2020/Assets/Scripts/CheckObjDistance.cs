49f3aec1f3d840caf95cf4448d52b6933f6b80a2
Jupiter98 <ale.sciarra@gmail.com>
2021-02-25 10:22:50 +0100

    Updated dice prefab with images and added simple script that checks for distance

---
+++ HCI_Project2020/Assets/Scripts/CheckObjDistance.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class CheckObjDistance : MonoBehaviour
+{
+    public Transform other;
+    public void SpawnAnimationEvent()
+    {
+        Debug.Log($"Spawn animation from {GetComponentInParent<Transform>().gameObject.name}");
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(CheckDistance),.5f,.5f);
+    }
+
+    private void CheckDistance()
+    {
+        if (other.GetComponent<MeshRenderer>().enabled == false) return;
+        var dist = Vector3.Distance(other.position, transform.position);
+        print($"Distance to other: {dist}");
+    }
+}
