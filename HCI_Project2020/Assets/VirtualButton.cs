30d30585b46b972d2df760e9f7b184db283a916f
Jupiter98 <ale.sciarra@gmail.com>
2021-03-24 18:46:27 +0100

    Added virtual button to the game

---
+++ HCI_Project2020/Assets/VirtualButton.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+public class VirtualButton : MonoBehaviour
+{
+    public GameObject virtualButton;
+    public UnityEvent onPushButton;
+    private void Start()
+    {
+        virtualButton.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(ButtonPressed);
+        virtualButton.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(ButtonReleased);
+    }
+
+    private void ButtonPressed(VirtualButtonBehaviour virtualButtonBehaviour)
+    {
+        Debug.Log("Premuto!");
+        onPushButton.Invoke();
+    }
+    private void ButtonReleased(VirtualButtonBehaviour virtualButtonBehaviour)
+    {
+        Debug.Log("Lasciato!");
+    }
+}
