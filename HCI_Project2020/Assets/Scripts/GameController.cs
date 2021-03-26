4b2a5e06ffc352a1f7a38ec3265259e0c846ba81
Jupiter98 <ale.sciarra@gmail.com>
2021-03-26 12:33:04 +0100

    Changed dice model spawning behaviour and created folder for URP settings

--- HCI_Project2020/Assets/Scripts/GameController.cs
+++ HCI_Project2020/Assets/Scripts/GameController.cs
@@ -1,6 +1,4 @@
 ﻿
-using System;
-using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,7 +7,9 @@ public class GameController : MonoBehaviour
     private static GameController _instance;
     [SerializeField] private GameObject firstTracker;
     [SerializeField] private GameObject secondTracker;
-    [SerializeField] private GameObject[] zephyrElements;
+    [SerializeField] private GameObject[] zephyrActiveElements;
+    [SerializeField] private GameObject[] zephyrPassiveElements;
+    [SerializeField] private GameObject[] zephyrGlassesElements;
     public static GameController Instance => _instance;
     private Transform _firstTrackerSpawnPos, _secondTrackerSpawnPos;
     public enum GameState
@@ -22,14 +22,6 @@ public class GameController : MonoBehaviour
 
     public GameState gameState = GameState.Default;
     
-    public enum SpawnedObjPool
-    {
-        Default,
-        Glasses,
-        Boy,
-        Girl,
-        Jewels,
-    }
     
     public enum Tracker
     {
@@ -81,26 +73,34 @@ public class GameController : MonoBehaviour
             switch (result)
             {
                 case 1:
-                    Debug.Log("spawn zephyr 1");
+                    Debug.Log("spawn active zephyr");
+                    obj = Instantiate(zephyrActiveElements[Random.Range(0,zephyrActiveElements.Length)].gameObject, _firstTrackerSpawnPos.position, Quaternion.identity);
+                    obj.transform.SetParent(_firstTrackerSpawnPos);
                     break;
                 case 2:
-                    Debug.Log("spawn zephyr 2");
-                    obj = Instantiate(zephyrElements[0].gameObject, _firstTrackerSpawnPos.position, Quaternion.identity);
+                    Debug.Log("spawn active zephyr");
+                    obj = Instantiate(zephyrActiveElements[Random.Range(0,zephyrActiveElements.Length)].gameObject, _firstTrackerSpawnPos.position, Quaternion.identity);
                     obj.transform.SetParent(_firstTrackerSpawnPos);
                     break;
                 case 3:
-                    Debug.Log("spawn zephyr 3");
+                    Debug.Log("spawn passive zephyr");
+                    obj = Instantiate(zephyrPassiveElements[Random.Range(0,zephyrPassiveElements.Length)].gameObject, _firstTrackerSpawnPos.position, Quaternion.identity);
+                    obj.transform.SetParent(_firstTrackerSpawnPos);
                     break;
                 case 4:
-                    Debug.Log("spawn zephyr 4");
+                    Debug.Log("spawn passive zephyr");
+                    obj = Instantiate(zephyrPassiveElements[Random.Range(0,zephyrPassiveElements.Length)].gameObject, _firstTrackerSpawnPos.position, Quaternion.identity);
+                    obj.transform.SetParent(_firstTrackerSpawnPos);
                     break;
                 case 5:
-                    Debug.Log("spawn zephyr 5");
-                    obj = Instantiate(zephyrElements[3].gameObject, _firstTrackerSpawnPos.position, Quaternion.identity);
+                    Debug.Log("spawn passive zephyr");
+                    obj = Instantiate(zephyrPassiveElements[Random.Range(0,zephyrPassiveElements.Length)].gameObject, _firstTrackerSpawnPos.position, Quaternion.identity);
                     obj.transform.SetParent(_firstTrackerSpawnPos);
                     break;
                 case 6:
-                    Debug.Log("spawn zephyr 6");
+                    Debug.Log("spawn active zephyr");
+                    obj = Instantiate(zephyrActiveElements[Random.Range(0,zephyrActiveElements.Length)].gameObject, _firstTrackerSpawnPos.position, Quaternion.identity);
+                    obj.transform.SetParent(_firstTrackerSpawnPos);
                     break;
             }
         }
@@ -108,27 +108,36 @@ public class GameController : MonoBehaviour
         {
             switch (result)
             {
+                //TODO change indexes 
                 case 1:
                     Debug.Log("spawn 1 glasses of alcohol");
+                    obj = Instantiate(zephyrGlassesElements[0].gameObject, _secondTrackerSpawnPos.position, Quaternion.identity);
+                    obj.transform.SetParent(_secondTrackerSpawnPos);
                     break;
                 case 2:
                     Debug.Log("spawn 2 glasses of alcohol");
-                    obj = Instantiate(zephyrElements[1].gameObject, _secondTrackerSpawnPos.position, Quaternion.identity);
+                    obj = Instantiate(zephyrGlassesElements[0].gameObject, _secondTrackerSpawnPos.position, Quaternion.identity);
                     obj.transform.SetParent(_secondTrackerSpawnPos);
                     break;
                 case 3:
                     Debug.Log("spawn 3 glasses of alcohol");
+                    obj = Instantiate(zephyrGlassesElements[0].gameObject, _secondTrackerSpawnPos.position, Quaternion.identity);
+                    obj.transform.SetParent(_secondTrackerSpawnPos);
                     break;
                 case 4:
                     Debug.Log("spawn 4 glasses of alcohol");
+                    obj = Instantiate(zephyrGlassesElements[0].gameObject, _secondTrackerSpawnPos.position, Quaternion.identity);
+                    obj.transform.SetParent(_secondTrackerSpawnPos);
                     break;
                 case 5:
                     Debug.Log("spawn 5 glasses of alcohol");
-                    obj = Instantiate(zephyrElements[2].gameObject, _secondTrackerSpawnPos.position, Quaternion.identity);
+                    obj = Instantiate(zephyrGlassesElements[0].gameObject, _secondTrackerSpawnPos.position, Quaternion.identity);
                     obj.transform.SetParent(_secondTrackerSpawnPos);
                     break;
                 case 6:
                     Debug.Log("spawn 6 glasses of alcohol");
+                    obj = Instantiate(zephyrGlassesElements[0].gameObject, _secondTrackerSpawnPos.position, Quaternion.identity);
+                    obj.transform.SetParent(_secondTrackerSpawnPos);
                     break;
             }
         }
